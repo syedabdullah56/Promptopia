968481841146b53964669b8fd0ec0e441a20b1cb
syedabdullah56 <syedabdullah8750@gmail.com>
2024-10-15 13:27:06 +0500

    First Commit After Production

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['lh3.googleusercontent.com'],  // Add external domains here
+    },
+  };
+  
+  export default nextConfig;
+  
