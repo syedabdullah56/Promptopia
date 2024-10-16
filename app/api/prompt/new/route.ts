8c5097f103454814a2088826acc4a51c5fd223c6
syedabdullah56 <syedabdullah8750@gmail.com>
2024-10-17 02:23:13 +0500

    fix 9

--- app/api/prompt/new/route.ts
+++ app/api/prompt/new/route.ts
@@ -1,22 +1,23 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const POST = async (req) => {
-    const {userId, prompt, tag} = await req.json();
+export const POST = async (req: Request) => {
+    const { userId, prompt, tag } = await req.json();
 
     try {
         await connectToDB();
         const newPrompt = new Prompt({
-            creator:userId,
+            creator: userId,
             prompt,
             tag
-        })
+        });
 
         await newPrompt.save();
-        return new Response(JSON.stringify(newPrompt),{status:201});
+        return new Response(JSON.stringify(newPrompt), { status: 201 });
     } catch (error) {
-        return new Response("Failed to create a new prompt",{    
-            status:500
-        })
+        console.error(error); // Log the error for debugging
+        return new Response("Failed to create a new prompt", {    
+            status: 500
+        });
     }
 }
