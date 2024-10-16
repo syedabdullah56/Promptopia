44865d97b63673153e399f224315364c6bb7b155
syedabdullah56 <syedabdullah8750@gmail.com>
2024-10-17 02:16:06 +0500

    fix 7

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -1,12 +1,23 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const GET = async () => {    //No need of request it is giving error in vercel defined but never use
+// export const GET = async () => {    //No need of request it is giving error in vercel defined but never use
+//     try {
+//         await connectToDB();
+//         const prompts = await Prompt.find({}).populate("creator");
+//         return new Response(JSON.stringify(prompts), { status: 200 });
+//     } catch (error) {
+//         return new Response("Failed to fetch all prompts", { status: 500 });
+//     }
+// }
+
+export const GET = async (request: Request, { params }: { params: { id: string } }) => {
     try {
         await connectToDB();
-        const prompts = await Prompt.find({}).populate("creator");
-        return new Response(JSON.stringify(prompts), { status: 200 });
+        const prompt = await Prompt.findById(params.id).populate("creator");
+
+        return new Response(JSON.stringify(prompt), { status: 200 });
     } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 });
+        return new Response("Failed to fetch prompt", { status: 500 });
     }
 }
